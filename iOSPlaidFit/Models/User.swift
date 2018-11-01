913ec1cbee2825b992d85bd898de6cbda3afadda
Winston Chu <wpchu@andrew.cmu.edu>
2018-11-01 19:50:29 -0400

    initial commit

---
+++ iOSPlaidFit/Models/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  iOSPlaidFit
+//
+//  Created by Winston Chu on 11/1/18.
+//  Copyright © 2018 Winston Chu. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    // MARK: - Properties
+    var id: Int?
+    var first_name: String?
+    var last_name: String?
+    var andrew_id: String?
+    var email: String?
+    var phone_number: String?
+    var role: String?
+    var year: String?
+    var missing_daily_boolean: Bool?
+    var missing_post_boolean: Bool?
+    var password: String?
+    var api_key: String?
+    
+    // MARK: - General
+    
+}
---
+++ iOSPlaidFit/View Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  iOSPlaidFit
+//
+//  Created by Winston Chu on 11/1/18.
+//  Copyright © 2018 Winston Chu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
