52b0dc51571df35e5b2b9b9b36a46b63b249ca2f
Winston Chu <wpchu@andrew.cmu.edu>
2018-11-01 19:36:38 -0400

    Initial Commit

---
+++ iOSPlaidFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPlaidFit
+//
+//  Created by Winston Chu on 11/1/18.
+//  Copyright © 2018 Winston Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
