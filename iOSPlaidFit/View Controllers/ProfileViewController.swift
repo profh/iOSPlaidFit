0ae7350568fe740c547b6c6b94bf0d9aa1a7e6a5
Winston Chu <wpchu@andrew.cmu.edu>
2018-11-18 13:45:18 -0500

    format phone number in profile page

--- iOSPlaidFit/View Controllers/ProfileViewController.swift
+++ iOSPlaidFit/View Controllers/ProfileViewController.swift
@@ -39,7 +39,7 @@ class ProfileViewController: UIViewController {
                 email.text = user.email
             }
             if let phone = self.phoneLabel {
-                phone.text = user.phone_number
+                phone.text = format(phoneNumber: user.phone_number!)
             }
             if let year = self.yearLabel {
                 year.text = user.year
@@ -52,6 +52,47 @@ class ProfileViewController: UIViewController {
         self.configureView()
     }
     
+    // https://stackoverflow.com/questions/32364055/formattting-phone-number-in-swift
+    func format(phoneNumber sourcePhoneNumber: String) -> String? {
+        // Remove any character that is not a number
+        let numbersOnly = sourcePhoneNumber.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        let length = numbersOnly.count
+        let hasLeadingOne = numbersOnly.hasPrefix("1")
+        // Check for supported phone number length
+        guard length == 7 || length == 10 || (length == 11 && hasLeadingOne) else {
+            return nil
+        }
+        let hasAreaCode = (length >= 10)
+        var sourceIndex = 0
+        // Leading 1
+        var leadingOne = ""
+        if hasLeadingOne {
+            leadingOne = "1 "
+            sourceIndex += 1
+        }
+        // Area code
+        var areaCode = ""
+        if hasAreaCode {
+            let areaCodeLength = 3
+            guard let areaCodeSubstring = numbersOnly.substring(start: sourceIndex, offsetBy: areaCodeLength) else {
+                return nil
+            }
+            areaCode = String(format: "(%@) ", areaCodeSubstring)
+            sourceIndex += areaCodeLength
+        }
+        // Prefix, 3 characters
+        let prefixLength = 3
+        guard let prefix = numbersOnly.substring(start: sourceIndex, offsetBy: prefixLength) else {
+            return nil
+        }
+        sourceIndex += prefixLength
+        // Suffix, 4 characters
+        let suffixLength = 4
+        guard let suffix = numbersOnly.substring(start: sourceIndex, offsetBy: suffixLength) else {
+            return nil
+        }
+        return leadingOne + areaCode + prefix + "-" + suffix
+    }
 
     // MARK: - Navigation
     
@@ -59,4 +100,20 @@ class ProfileViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
 
+}
+
+// Used to formatting the phone number string
+extension String {
+    /// This method makes it easier extract a substring by character index where a character is viewed as a human-readable character (grapheme cluster).
+    internal func substring(start: Int, offsetBy: Int) -> String? {
+        guard let substringStartIndex = self.index(startIndex, offsetBy: start, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        guard let substringEndIndex = self.index(startIndex, offsetBy: start + offsetBy, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        return String(self[substringStartIndex ..< substringEndIndex])
+    }
 }
