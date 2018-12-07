85a3515f2c4d32743e0e61f9b9e0467db3730f6a
Winston Chu <wpchu@andrew.cmu.edu>
2018-12-07 08:49:41 -0500

    successfully refactored the sleep view controller

---
+++ iOSPlaidFit/Models/ApiUrl.swift
@@ -0,0 +1,15 @@
+//
+//  ApiUrl.swift
+//  iOSPlaidFit
+//
+//  Created by Winston Chu on 12/7/18.
+//  Copyright © 2018 Winston Chu. All rights reserved.
+//
+
+import Foundation
+
+class ApiUrl {
+    
+    let get_user_url = "http://localhost:3000/v1/users/"
+    
+}
